28ea9947546cf1e6b6267676278e460e0456ede0
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-17 01:06:04 +0600

    Navbar added with logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div>
-      
+      <Home></Home>
+      <Routes>
+
+      </Routes>
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './Header.css';
+import logo from '../../../images/logo-digi.png';
 const Header = () => {
     return (
         <div>
-            <h2>This is header</h2>
+            <Navbar className='' style={{ backgroundColor: "" }} collapseOnSelect expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img className='w-75' src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="">Blogs</Nav.Link>
+                            <Nav.Link href="">About Me</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Login
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
