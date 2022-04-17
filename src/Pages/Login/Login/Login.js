cbc7937326fc4a9afe64e94893b62e53ee89c722
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-17 15:55:57 +0600

    Login form design completed

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is login page</h2>
+        <div className='w-50 mx-auto vh-100 mt-5'>
+            <Container>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button id='login-btn' variant="w-50 mx-auto d-block mb-2" type="submit">
+                        Login
+                    </Button>
+                    <p>New in Digi Mark? <Link to='/' className='text-decoration-none'>Create an account</Link></p>
+                    <p>Forget Password? <Link to='/' className='text-decoration-none'>Reset Password</Link></p>
+                </Form>
+            </Container>
         </div>
     );
 };
