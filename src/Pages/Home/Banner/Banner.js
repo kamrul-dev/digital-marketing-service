e18514ab52dda9d80d20000984b7010a3ad12f4d
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-18 17:41:57 +0600

    Banner section updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
+import './Banner.css';
 import banner1 from '../../../images/Banner/banner1.jpg';
+import banner2 from '../../../images/Banner/banner2.jpg';
+import banner3 from '../../../images/Banner/banner3.jpg';
 
 //Carouse Slider in hompage
 const Banner = () => {
@@ -10,7 +13,7 @@ const Banner = () => {
         setIndex(selectedIndex);
     };
     return (
-        <div>
+        <div className='banner-image'>
             <Carousel activeIndex={index} onSelect={handleSelect}>
                 <Carousel.Item>
                     <img
@@ -18,32 +21,20 @@ const Banner = () => {
                         src={banner1}
                         alt="First slide"
                     />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src={banner1}
+                        src={banner2}
                         alt="Second slide"
                     />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src={banner1}
+                        src={banner3}
                         alt="Third slide"
                     />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
         </div>
