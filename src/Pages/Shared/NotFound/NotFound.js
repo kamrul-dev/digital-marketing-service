0380a69a1d5cbdf72c6a9fd1f6f9dcedcf618d40
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-18 10:05:51 +0600

    404 Not found page completed

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './NotFound.css';
+import NotFoundImage from '../../../images/not-found.png';
+import { Link } from 'react-router-dom';
 const NotFound = () => {
     return (
-        <div>
-            <h2>This 404 Not Found</h2>
+        <div className='row container mx-auto notfound-section vh-100'>
+            <div className='col-md-6'>
+                <div className='text-center'>
+                    <h2>Opps!!</h2>
+                    <Link to='/'>
+                        <button className='btn btn-warning'>Back to Home</button>
+                    </Link>
+                </div>
+            </div>
+            <div className='col-md-6'>
+                <div className='notfound-img  text-center'>
+                    <img src={NotFoundImage} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
